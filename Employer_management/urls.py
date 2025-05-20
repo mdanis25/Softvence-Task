30920007c517b4e75bdd7fd0145d7b0e6096178e
Md Anis <rjanis2083@gmail.com>
2025-05-21 03:08:50 +0600

    Add employer URLs to API app routing

--- Employer_management/urls.py
+++ Employer_management/urls.py
@@ -2,8 +2,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('accounts.urls')), 
+    path('admin/', admin.site.urls),  
+    path('api/', include('api.urls')),  
     
 ]
--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser 
 
-# Register your models here.
+@admin.register(CustomUser) 
+class CustomUserAdmin(UserAdmin): 
+    list_display = ['id','email', 'username', 'password', 'is_staff', 'is_active']
+    
